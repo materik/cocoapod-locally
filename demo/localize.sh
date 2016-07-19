fc53046c8c9ebd4b7c21b695193ef75c8c00c39a
Mattias Eriksson <thematerik@gmail.com>
2016-07-19 20:18:58 +0200

    [test] fix failing tests

--- demo/localize.sh
+++ demo/localize.sh
@@ -3,9 +3,3 @@
 ./bin/locally.js -i demo/Demo -o demo/Demo/en.lproj/Localizable.strings
 ./bin/locally.js -i demo/Demo -o demo/Demo/sv.lproj/Localizable.strings
 
-./bin/locally.js \
-    -i demo/Demo \
-    -o demo/Demo/en.lproj/Localizable.strings \
-    -p "NSLocalizedString(@(), .*)" \
-    -p "@().localize" \
-
