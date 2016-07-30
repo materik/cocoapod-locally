61037d58d65f9ee80f36514a7b725cd2f2db1bac
Mattias Eriksson <thematerik@gmail.com>
2016-07-30 10:35:33 +0200

    [test] change how the test is executed

--- test/test.js
+++ test/test.js
@@ -4,82 +4,99 @@ var child_process = require('child_process');
 var diff = require('file-compare').compare;
 var expect = require('chai').expect;
 var fs = require('fs');
-var ncp = require('ncp').ncp;
 
-var reset = function(step, callback) {
-    resetLang('en', step, function() {
-        resetLang('sv', step, function() {
-            resetImplementationFile(step, callback);
+var setupTest = function(test, callback, langs) {
+    langs = _.clone(langs) || ['en'];
+    var lang = langs.pop();
+    if (lang) {
+        setupLang(lang, test, function() {
+            setupTest(test, callback, langs);
         });
-    });
+    } else {
+        setupImplementationFile(test, callback);
+    }
 }
 
-var resetLang = function(lang, step, callback) {
-    var localizableFile = lang + '.lproj/Localizable.strings';
-    var testFile = './test/' + step + '/Before/' + localizableFile;
-    var demoFile = './demo/Demo/' + localizableFile;
-    child_process.exec('cp ' + testFile + ' ' + demoFile, function(err) {
+var setupLang = function(lang, test, callback) {
+    var localizableFile = lang + '.lproj';
+    var expectedFile = './test/' + test + '/Before/' + localizableFile;
+    var actualFile = './test/Test/';
+    child_process.exec('cp -r ' + expectedFile + ' ' + actualFile, function(err) {
         expect(err).to.be.null;
         callback();
     });
 }
 
-var resetImplementationFile = function(step, callback) {
+var setupImplementationFile = function(test, callback) {
     var file = 'ViewController.m';
-    var testFile = './test/' + step + '/' + file;
-    var demoFile = './demo/Demo/' + file;
-    child_process.exec('cp ' + testFile + ' ' + demoFile, function(err) {
+    var expectedFile = './test/' + test + '/' + file;
+    var actualFile = './test/Test/' + file;
+    child_process.exec('cp -r ' + expectedFile + ' ' + actualFile, function(err) {
         expect(err).to.be.null;
         callback();
     });
 }
 
-var compare = function(step, callback) {
-    compareLang('en', step, function() {
-        compareLang('sv', step, callback);
-    });
-}
-
-var compareLang = function(lang, step, callback) {
-    var localizableFile = lang + '.lproj/Localizable.strings';
-    var testFile = './test/' + step + '/After/' + localizableFile;
-    var demoFile = './demo/Demo/' + localizableFile;
-    diff(testFile, demoFile, function(result) {
-        if (!result) {
-            var testFileContent = fs.readFileSync(testFile, 'utf8');
-            var demoFileContent = fs.readFileSync(demoFile, 'utf8');
-            expect(demoFileContent).to.be.eql(testFileContent);
-        }
-
-        callback();
-    });
-}
-
-var run = function(callback) {
-    child_process.exec('npm run demo', function(err, stdout) {
-        expect(err).to.be.null;
-        callback();
-    });
-}
-
-var runSimpleTest = function(step, callback) {
-    reset(step, function() {
-        run(function() {
-            compare(step, callback);
+var compare = function(test, callback, langs) {
+    langs = _.clone(langs) || ['en'];
+    var lang = langs.pop();
+    if (lang) {
+        compareLang(lang, test, function() {
+            compare(test, callback, langs);
         });
+    } else {
+        callback();
+    }
+}
+
+var compareLang = function(lang, test, callback) {
+    var localizableFile = lang + '.lproj/Localizable.strings';
+    var expectedFile = './test/' + test + '/After/' + localizableFile;
+    var actualFile = './test/Test/' + localizableFile;
+    diff(expectedFile, actualFile, function(result) {
+        if (!result) {
+            var expectedFileContent = fs.readFileSync(expectedFile, 'utf8');
+            var actualFileContent = fs.readFileSync(actualFile, 'utf8');
+            expect(actualFileContent).to.be.eql(expectedFileContent);
+        }
+        callback();
     });
+}
+
+var run = function(callback, langs) {
+    langs = _.clone(langs) || ['en'];
+    var bin = './bin/locally.js';
+    var input = 'test/Test';
+    var outputs = langs.map(function(lang) {
+        return 'test/Test/' + lang + '.lproj/Localizable.strings';
+    });
+    var cmd = bin + ' -i ' + input + ' -o ' + outputs.join(' -o ');
+    child_process.exec(cmd, function(err, stdout) {
+        expect(err).to.be.null;
+        callback();
+    });
+}
+
+var runSimpleTest = function(test, callback, langs) {
+    langs = _.clone(langs) || ['en'];
+    setupTest(test, function() {
+        run(function() {
+            compare(test, callback);
+        }, langs);
+    }, langs);
 }
 
 describe('compare', function() {
 
-    after(function(callback) { reset('1', callback); });
+    before(function(callback) { child_process.exec('mkdir test/Test', callback); });
+    after(function(callback) { child_process.exec('rm -r test/Test', callback); });
 
-    it('run once', function(callback) {
+    it('once', function(callback) {
         runSimpleTest('1', callback);
     });
 
-    it('run twice', function(callback) {
-        reset('2', function() {
+    it('twice', function(callback) {
+        setupTest('2', function() {
             run(function() {
                 run(function() {
                     compare('2', callback);
@@ -88,24 +105,28 @@ describe('compare', function() {
         });
     });
 
-    it('run while missing ignored strings', function(callback) {
+    it('while missing ignored strings', function(callback) {
         runSimpleTest('3', callback);
     });
 
-    it('run with empty strings in the Localizable file', function(callback) {
+    it('with empty strings in the Localizable file', function(callback) {
         runSimpleTest('4', callback);
     });
 
-    it('run with sorted result', function(callback) {
+    it('with sorted result', function(callback) {
         runSimpleTest('5', callback);
     });
 
-    it('run with quotes in the localized value', function(callback) {
+    it('with quotes in the localized value', function(callback) {
         runSimpleTest('6', callback);
     });
 
-    it('run with to localization expressions on the same line', function(callback) {
+    it('with to localization expressions on the same line', function(callback) {
         runSimpleTest('7', callback);
+    });
+
+    it('multiple languages', function(callback) {
+        runSimpleTest('8', callback, ['en', 'sv', 'es']);
     });
 
 });
